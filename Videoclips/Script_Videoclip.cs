6b1c873d43ed5cc5abae23d0a5fefa9022368095
Jose Ángel <joseangel@Joses-MacBook-Pro.local>
2024-02-21 22:17:44 +0100

    videoclip by url

--- Videoclips/Script_Videoclip.cs
+++ Videoclips/Script_Videoclip.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+
 
 public class Script_Videoclip : MonoBehaviour
 {
     public Material videoMaterial; // Asigna aquí el material del video
-    // public RenderTexture videoTexture; // Asigna aquí tu Render Texture
 
+    // AVISO: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Ruta al video dentro de los Assets del proyecto
+    // Ruta donde poner los videos:
+    // MiNombreProyecto/Assets/StreamingAssets/Sepinaco/Videoclips/DELAOSSA - JUDAS (PROD. JMOODS).mp4
+    // Ruta para poner aqui en el codigo:
+    // Sepinaco/Videoclips/DELAOSSA - JUDAS (PROD. JMOODS).mp4
+    private string videoPath = "Sepinaco/Videoclips/DELAOSSA - JUDAS (PROD. JMOODS).mp4";
+    private VideoPlayer videoPlayer;
 
-    // Start is called before the first frame update
     void Start()
     {
         ReplaceSceneMaterials(videoMaterial);
-        // Material videoMaterial = new Material(Shader.Find("Unlit/Texture"));
-        // videoMaterial.mainTexture = videoTexture; // Usa la Render Texture como la textura principal del material
-        // ReplaceSceneMaterials(videoMaterial);
+        StartVideoPlayer();
     }
 
     void ReplaceSceneMaterials(Material newMaterial)
@@ -31,9 +37,56 @@ public class Script_Videoclip : MonoBehaviour
         }
     }
 
+    void StartVideoPlayer() {
+        // Obtén el componente VideoPlayer ya existente en este GameObject
+        videoPlayer = GetComponent<VideoPlayer>();
+        if (videoPlayer == null)
+        {
+            Debug.LogError("No se encontró el componente VideoPlayer en el GameObject.");
+            return;
+        }
+
+        // Configura el VideoPlayer
+        // videoPlayer.playOnAwake = false;
+        // videoPlayer.renderMode = VideoRenderMode.MaterialOverride;
+        // videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+        // videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        // videoPlayer.SetTargetAudioSource(0, GetComponent<AudioSource>());
+
+        // Establece el clip de video
+        SetVideoClip(videoPath);
+
+        // Opcional: Reproducir automáticamente
+        videoPlayer.Play();
+    }
+
+    void SetVideoClip(string path)
+    {
+        // https://docs.unity3d.com/Manual/StreamingAssets.html
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, path);
+    }
+
     // Update is called once per frame
     void Update()
     {
         
     }
 }
+
+
+
+
+// OLD CODE
+/*
+    // public RenderTexture videoTexture; // Asigna aquí tu Render Texture
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReplaceSceneMaterials(videoMaterial);
+        // Material videoMaterial = new Material(Shader.Find("Unlit/Texture"));
+        // videoMaterial.mainTexture = videoTexture; // Usa la Render Texture como la textura principal del material
+        // ReplaceSceneMaterials(videoMaterial);
+    }
+*/
